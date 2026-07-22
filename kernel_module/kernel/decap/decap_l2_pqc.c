8fd06b47555f5eeb93b983e8b0fe3a8bea58a11d
Buuhis <tobuu213@gmail.com>
2026-07-22 21:33:09 +0700

    fix: failed decrypt in rx with err=-74 v1.0.2

--- kernel_module/kernel/decap/decap_l2_pqc.c
+++ kernel_module/kernel/decap/decap_l2_pqc.c
@@ -135,11 +135,22 @@ static int l2_pqc_decrypt_skb(struct sk_buff *skb)
     }
 
     {
-        struct scatterlist sg[2];
+        struct scatterlist sg[MAX_SKB_FRAGS + 3];
+        int nents;
 
-        sg_init_table(sg, 2);
+        sg_init_table(sg, ARRAY_SIZE(sg));
         sg_set_buf(&sg[0], skb->data, MWAN_CRYPTO_HDR_LEN); // AAD
-        sg_set_buf(&sg[1], skb->data + MWAN_CRYPTO_HDR_LEN, ciphertext_len); // Ciphertext + Tag
+
+        nents = skb_to_sgvec(skb, &sg[1], MWAN_CRYPTO_HDR_LEN, ciphertext_len);
+        if (unlikely(nents < 0)) {
+            pr_warn("mwan_kmod DBG Decap: skb_to_sgvec failed, nents=%d\n", nents);
+            aead_request_free(req);
+            rcu_read_unlock();
+            if (pulled_bytes > 0) {
+                skb_push(skb, pulled_bytes);
+            }
+            return -EINVAL;
+        }
 
         aead_request_set_crypt(req, sg, sg, ciphertext_len, iv_buf);
         aead_request_set_ad(req, MWAN_CRYPTO_HDR_LEN);
